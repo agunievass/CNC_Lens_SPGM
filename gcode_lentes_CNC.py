95f96822aa747ea7fcb627411b016a21b3c985f4
agunievass <62716543+agunievass@users.noreply.github.com>
2023-02-04 02:06:30 -0300

    New code by bro, and matplotlib graphing
    new code supplyed by brother, worked nice, added matplotlib graph, tho gcode generation still not working as needed

--- gcode_lentes_CNC.py
+++ gcode_lentes_CNC.py
@@ -1,29 +1,45 @@
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 #Variables a introducir
-#Es MUY importante que los valores esten bien calculados; revisar el RADIO y H y L, deben coincidir como en el solidworks
-#Si los resultados no te dan coherentes al ejecutar el programa, revisar estos parametros iniciales.
-r = 39 #Radius of curvature of the lens
-H = 3 #Height of the lens (thickness if you want)
-L = 15 #Lenght of triangle formed (wideness of the lens)
-DOF = 0.1 #Depth Of Cut
-Z = 25 #Safe zone in X for the tool to stand by
-feedrate = 120
+r = 52.5 #Radius
+#H = 4 #Height of lens
+#DOC = 0.05 #Depth of cut
+feedrate = 105 #speed of the machine
+safezone = 22 #safe zone in X axis for the tool to rest
+angle =19.46
 
-with open('lenteV1.txt', 'w') as f: #Open the file in order to write the GCODE to it
-    f.write("G1 "+ "F"+ str(feedrate) +" "+ "X" + str(Z)) #go to safe position
-    f.write('\n')
-    for i in range(int(H/DOF)):
-        pasada = round(i*DOF, 1) #Profundidad de pasada
-        longitud_pasada = np.sqrt(r**2-(r-(H-pasada))**2) #longitud de la pasada
-        print("Distancia:", round(abs(L-longitud_pasada), 2), "   ", round(pasada, 2)) #imprimo el valor; solo para demostracion
-        f.write("G1 "+ "Y-" + str(round(pasada,2))) #Go on Y direction (depth of pass)
-        f.write('\n')
-        f.write("G1 " + "X" + str(round(abs(L-longitud_pasada), 2))) #Go on X direction (shape of lens)
-        f.write('\n')
-        f.write("G0 X"+ str(Z))
-        f.write('\n')
-print("Number of operations: "+ str(round(H/DOF)))
+x_array = np.array([])
+y_array = np.array([])
 
-#with open('lenteV1.txt', 'w') as f:
+
+with open('lenteV2.txt', 'w') as f:
+    #Tells the machine to go to safezone at feedrate speeds
+    f.write("G1 F" + str(feedrate) + " X" + str(safezone) + '\n')
+    print("X    Y")
+    for i in np.arange(0, np.pi/2, 0.1):
+        x = r * math.sin(i)
+        y = r * math.cos(i)
+        #f.write("G0 " + "X" + str(safezone) + '\n') #go to safe zone again, come back to safe zone
+        #f.write("G1 " + "Y-" + str(round(y,2)) + '\n') #go deeper onto the material
+        #f.write("G1 " + "X" + str(round(x,2)) + '\n') #go brrr into the material
+        x_array = np.append(x_array, [round(x, 2)])
+        y_array = np.append(y_array, [round(y, 2)])
+        #print("X: " + str(x) + "     " + "Y: " + str(y) )
+        plt.plot(x_array, y_array)
+
+#print("X    Y")
+#for i in x_array:
+#    print(i, end = ' ')
+#for w in y_array:
+#    print(w)
+
+
+print("Numero de pasos: " +str(len(x_array)))
+plt.title("Curve plotted using the given points")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.show()
+
+#with open('lenteV2.txt', 'w') as f:
 #    f.write("aguante boca")
